ca21ff218cc898a4c3e896ee892b809f16647523
Matt Riese <rise.matt@gmail.com>
2021-05-18 16:58:57 -0700

    added schema and 2 tests for bad requests

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -15,4 +15,41 @@ describe("POST /", function () {
 
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
   });
+
+  test("invalid json data (one field)", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 100,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "12345-6789",
+    });
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance.productId must be greater than or equal to 1000"
+        ],
+        "status": 400
+      }
+    });
+  });
+
+    test("invalid json data (all fields)", async function () {
+      const resp = await request(app).post("/shipments").send({
+        productId: 100,
+        name: "",
+        addr: 100
+      });
+      expect(resp.body).toEqual({
+        "error": {
+          "message": [
+            "instance.productId must be greater than or equal to 1000",
+            "instance.name does not meet minimum length of 1",
+            "instance.addr is not of a type(s) string",
+            "instance requires property \"zip\""
+          ],
+          "status": 400
+        }
+      });
+  });
+
 });
